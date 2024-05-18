e0cbc83faec1016019ae52f7a3cb3d170ae4a69a
Martin Juul <code@juul.xyz>
2024-05-19 00:13:04 +0200

    added rss feed

---
+++ resources/app/rss-feeds/rss-feed-viewer.tsx
@@ -0,0 +1,13 @@
+import { RssFeed } from '@/rss-feeds/models.ts';
+
+type Props = {
+  feed: RssFeed;
+}
+const RssFeedViewer = ({}: Props) => {
+
+  return (
+    <></>
+  )
+};
+
+export default RssFeedViewer;
